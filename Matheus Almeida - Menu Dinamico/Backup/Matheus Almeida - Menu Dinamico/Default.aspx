73af74a975fd1dcb03a9dfac8d029a622c926d61
man7698 <matheusalmeida7698@gmail.com>
2021-06-17 00:21:18 -0300

    Add files via upload

---
+++ Matheus Almeida - Menu Dinamico/Backup/Matheus Almeida - Menu Dinamico/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Matheus_Almeida___Menu_Dinamico.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="Style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Menu ID="Menu1" runat="server" Orientation="Horizontal" >
+
+        <LevelMenuItemStyles>
+    <asp:MenuItemStyle CssClass="level1"/>
+    <asp:MenuItemStyle CssClass="level2"/>
+    <asp:MenuItemStyle CssClass="level3" />
+    <asp:MenuItemStyle CssClass="level4" />
+  </LevelMenuItemStyles>
+  
+  <StaticHoverStyle CssClass="hoverstyle"/>
+
+        </asp:Menu>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Matheus Almeida - Menu Dinamico/Backup/Matheus Almeida - Menu Dinamico/Default.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+namespace Matheus_Almeida___Menu_Dinamico
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                carregaMenu2();
+        }
+
+        private void carregaMenu2()
+        {
+            using (SqlConnection conn = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=trabalhos;Data Source=."))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("select menuid, descricao,link, parentid  from MENU order by menuid", conn))
+                {
+                    SqlDataReader dtr = cmd.ExecuteReader();
+                    while (dtr.Read())
+                    {
+                        MenuItem mi = null;
+
+                        mi = new MenuItem(dtr["descricao"].ToString(), dtr["menuid"].ToString());
+
+                        //Verifica se a linha é raiz
+                        if (dtr["parentid"] == DBNull.Value)
+                        {
+                            //Adiciona um novo nó no Menu
+
+                            Menu1.Items.Add(mi);
+                        }
+                        else
+                        {
+                            miPaiNo = null;
+                            MenuItem miPai = localizaMenuPai(Menu1.Items, (int)dtr["parentid"]);
+                            mi.Text = dtr["descricao"].ToString();
+                            mi.NavigateUrl = dtr["link"].ToString();
+                            if (miPai != null) miPai.ChildItems.Add(mi);
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+
+
+
+        static MenuItem miPaiNo = null;
+
+        private MenuItem localizaMenuPai(MenuItemCollection menuItemCollection, int PaiId)
+        {
+            foreach (MenuItem mi in menuItemCollection)
+            {
+                if (mi.Value.Equals(PaiId.ToString()))
+                    miPaiNo = mi;
+                else
+                    if (mi.ChildItems != null)
+                        this.localizaMenuPai(mi.ChildItems, PaiId);
+            }
+            return miPaiNo;
+        }
+    }
+}
